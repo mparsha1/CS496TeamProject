565d58b1ae4a0a90d34a516282876d2c9d6b3aba
Drew Holtzapple <dholtzap@ycp.edu>
2014-04-04 15:20:59 -0400

    login servlet 80% done

--- CS496CollegePlannerWebService/src/edu/ycp/cs/cs496/collegeplanner/servlets/LoginServlet.java
+++ CS496CollegePlannerWebService/src/edu/ycp/cs/cs496/collegeplanner/servlets/LoginServlet.java
@@ -8,14 +8,39 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import edu.ycp.cs.cs496.collegeplanner.controllers.LoginController;
+import edu.ycp.cs.cs496.collegeplanner.models.User;
 
 public class LoginServlet extends HttpServlet{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		LoginController controller = new LoginController();
 		
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		
+		User user = new User();
+		
+		user.setUsername(username);
+		user.setPassword(password);
+		
+		boolean result = controller.login(user);
+		
+		resp.setContentType("application/json");
+		resp.setStatus(HttpServletResponse.SC_OK);
+		
+		if(result == true) {
+						
+						
+			return;
+		}
+		
+		
+		
 	}
 }
