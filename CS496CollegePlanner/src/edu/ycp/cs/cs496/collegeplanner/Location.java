faeb76446c4108c3284263ca811a4cb90d9b0b73
Drew Holtzapple <dholtzap@ycp.edu>
2014-04-10 11:19:41 -0400

    merged - weird file bug

---
+++ CS496CollegePlanner/src/edu/ycp/cs/cs496/collegeplanner/Location.java
@@ -0,0 +1,49 @@
+package edu.ycp.cs.cs496.collegeplanner;
+
+/**
+ * @author dholtzap
+ * class to keep track of information pertaining to locations 
+ * at college/university
+ */
+public class Location {
+	private int id;
+	private String campus;
+	private String building;
+	private int roomNumber;
+	
+	public Location() {
+		
+	}
+	
+	public String getCampus() {
+		return campus;
+	}
+	
+	public void setCampus(String campus) {
+		this.campus = campus;
+	}
+	
+	public int getRoomNumber() {
+		return roomNumber;
+	}
+	
+	public void setRoomNumber(int roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+	
+	public String getBuilding() {
+		return building;
+	}
+	
+	public void setBuilding(String building) {
+		this.building = building;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+}
---
+++ CS496CollegePlanner/src/edu/ycp/cs/cs496/collegeplanner/User.java
@@ -0,0 +1,70 @@
+package edu.ycp.cs.cs496.collegeplanner;
+
+import java.util.ArrayList;
+/**
+ * 
+ * @author dholtzap
+ * store all information pertaining to users
+ */
+public class User {
+	private int id;
+	private String username;
+	private String password;
+	private String emailAddress;
+	private ArrayList<Course> courses; //FIXME: other possible better data-type?
+	private String major;
+	
+	// TODO: field for subscriptions after subscriptions class made
+	public User() {
+		
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public ArrayList<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(ArrayList<Course> courses) {
+		this.courses = courses;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}	
+	
+	public String getMajor() {
+		return major;
+	}
+	
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	
+}
